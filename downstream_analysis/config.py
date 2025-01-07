e82a27232d35280837b3f16cc3141c74cd6fd040
Francesco Craighero <francesco.craighero@epfl.ch>
2025-01-07 17:34:31 +0100

    updated config

--- downstream_analysis/config.py
+++ downstream_analysis/config.py
@@ -1,11 +1,27 @@
 from pyhere import here
 
+# Font sizes
+FSS = 5
+FSM = 6
+FSB = 7
+
+# Figure width
 TEXTWIDTH_CM = 18
 CM_TO_INCH = 1 / 2.54
 TEXTWIDTH_INCH = TEXTWIDTH_CM * CM_TO_INCH
 
+# File paths
 GENCODE_FPATH = here("data", "data", "genetic_code.csv")
 ENSEMBL_FPATH = here("data", "data", "ensembl.cds.fa")
+
+# File names
+ATTR_FNAMES = [
+    "241126_RDHPLG_extraLong_int.h5",
+    "241001_RDHPLG_test_int.h5",
+    "241022_RDHPLG_train_int.h5",
+    "241029_RDHPLG_val_int.h5",
+    "241105_RDHPLG_extra_int.h5",
+]
 
 DISCARDED_TRANSCRIPTS = [
     "ENSMUST00000000828.12",
@@ -16,12 +32,20 @@ DISCARDED_TRANSCRIPTS = [
     "ENSMUST00000145167.7",
 ]
 
-ATTR_FNAMES = [
-    "241126_RDHPLG_extraLong_int.h5",
-    "241001_RDHPLG_test_int.h5",
-    "241022_RDHPLG_train_int.h5",
-    "241029_RDHPLG_val_int.h5",
-    "241105_RDHPLG_extra_int.h5",
-]
-
 SAVEFIG_KWARGS = dict(dpi=600, bbox_inches="tight", pad_inches=0.0)
+
+CONDITIONS_FIXNAME = {
+        'CTRL': 'CTRL',
+        'VAL': 'VAL (V)',
+        'ILE': 'ILE (I)',
+        'LEU': 'LEU (L)',
+        'LEU_ILE': '(L, I)', 
+        'LEU_ILE_VAL': '(L, I, V)'}
+
+def rgb_to_rgb01(rgb: tuple[int]):
+    return tuple([c / 255 for c in rgb])
+
+
+ATTR_COL = rgb_to_rgb01((253, 180, 98))
+TRUE_COL = rgb_to_rgb01((128, 177, 211))
+PRED_COL = rgb_to_rgb01((141, 211, 199))
